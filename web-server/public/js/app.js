a331a8ce547298542b7fe99b88b01b05faa94c53
Celik Berk Altingok <cbaltingok@gmail.com>
2021-01-09 21:58:46 +0300

    indent

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -10,7 +10,6 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Fetching Data'
     messageTwo.textContent = ''
 
-
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if(data.error) {
